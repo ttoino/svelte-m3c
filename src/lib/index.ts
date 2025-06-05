40f6082b2b033006196f74f3ffe0822abb77677b
toino <me@toino.pt>
2025-06-05 21:50:10 +0100

    Only export components from the top level

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,143 +1,67 @@
-import Button, { variants as buttonVariants } from "./components/Button.svelte";
-import Checkbox, {
-    variants as checkboxVariants,
-} from "./components/Checkbox.svelte";
-import Divider, {
-    variants as dividerVariants,
-} from "./components/Divider.svelte";
-import Icon, { variants as iconVariants } from "./components/Icon.svelte";
-import IconButton, {
-    variants as iconButtonVariants,
-} from "./components/IconButton.svelte";
-import List, { variants as listVariants } from "./components/List.svelte";
-import ListItem, {
-    variants as listItemVariants,
-} from "./components/ListItem.svelte";
+import Button from "./components/Button.svelte";
+import Checkbox from "./components/Checkbox.svelte";
+import Divider from "./components/Divider.svelte";
+import Icon from "./components/Icon.svelte";
+import IconButton from "./components/IconButton.svelte";
+import List from "./components/List.svelte";
+import ListItem from "./components/ListItem.svelte";
 import Menu from "./components/Menu.svelte";
-import MenuDivider, {
-    variants as menuDividerVariants,
-} from "./components/MenuDivider.svelte";
-import MenuItem, {
-    variants as menuItemVariants,
-} from "./components/MenuItem.svelte";
-import MenuList, {
-    variants as menuListVariants,
-} from "./components/MenuList.svelte";
+import MenuDivider from "./components/MenuDivider.svelte";
+import MenuItem from "./components/MenuItem.svelte";
+import MenuList from "./components/MenuList.svelte";
 import MenuTrigger from "./components/MenuTrigger.svelte";
-import NavigationDrawer, {
-    variants as navigationDrawerVariants,
-} from "./components/NavigationDrawer.svelte";
-import NavigationDrawerItem, {
-    variants as navigationDrawerItemVariants,
-} from "./components/NavigationDrawerItem.svelte";
-import NavigationRail, {
-    variants as navigationRailVariants,
-} from "./components/NavigationRail.svelte";
-import NavigationRailItem, {
-    variants as navigationRailItemVariants,
-} from "./components/NavigationRailItem.svelte";
-import Progress, {
-    variants as progressVariants,
-} from "./components/Progress.svelte";
+import NavigationDrawer from "./components/NavigationDrawer.svelte";
+import NavigationDrawerItem from "./components/NavigationDrawerItem.svelte";
+import NavigationRail from "./components/NavigationRail.svelte";
+import NavigationRailItem from "./components/NavigationRailItem.svelte";
+import Progress from "./components/Progress.svelte";
 import Provider from "./components/Provider.svelte";
-import Radio, { variants as radioVariants } from "./components/Radio.svelte";
+import Radio from "./components/Radio.svelte";
 import RadioGroup from "./components/RadioGroup.svelte";
-import ScrollArea, {
-    variants as scrollAreaVariants,
-} from "./components/ScrollArea.svelte";
-import SearchBar, {
-    variants as searchBarVariants,
-} from "./components/SearchBar.svelte";
-import SegmentedButton, {
-    variants as segmentedButtonVariants,
-} from "./components/SegmentedButton.svelte";
-import SegmentedButtonGroup, {
-    variants as segmentedButtonGroupVariants,
-} from "./components/SegmentedButtonGroup.svelte";
-import Slider, { variants as sliderVariants } from "./components/Slider.svelte";
-import StateLayer, {
-    variants as stateLayerVariants,
-} from "./components/StateLayer.svelte";
-import Switch, { variants as switchVariants } from "./components/Switch.svelte";
-import TextField, {
-    variants as textFieldVariants,
-} from "./components/TextField.svelte";
-import ToggleIconButton, {
-    variants as toggleIconButtonVariants,
-} from "./components/ToggleIconButton.svelte";
-import Tooltip, {
-    variants as tooltipVariants,
-} from "./components/Tooltip.svelte";
+import ScrollArea from "./components/ScrollArea.svelte";
+import SearchBar from "./components/SearchBar.svelte";
+import SegmentedButton from "./components/SegmentedButton.svelte";
+import SegmentedButtonGroup from "./components/SegmentedButtonGroup.svelte";
+import Slider from "./components/Slider.svelte";
+import StateLayer from "./components/StateLayer.svelte";
+import Switch from "./components/Switch.svelte";
+import TextField from "./components/TextField.svelte";
+import ToggleIconButton from "./components/ToggleIconButton.svelte";
+import Tooltip from "./components/Tooltip.svelte";
 import TooltipRoot from "./components/TooltipRoot.svelte";
 import TooltipTrigger from "./components/TooltipTrigger.svelte";
-import {
-    cssPaletteFromColor,
-    cssPaletteFromImage,
-    tailwindPaletteFromColor,
-    tailwindPaletteFromImage,
-} from "./palette.js";
 
 export {
     Button,
-    buttonVariants,
     Checkbox,
-    checkboxVariants,
-    cssPaletteFromColor,
-    cssPaletteFromImage,
     Divider,
-    dividerVariants,
     Icon,
     IconButton,
-    iconButtonVariants,
-    iconVariants,
     List,
     ListItem,
-    listItemVariants,
-    listVariants,
     Menu,
     MenuDivider,
-    menuDividerVariants,
     MenuItem,
-    menuItemVariants,
     MenuList,
-    menuListVariants,
     MenuTrigger,
     NavigationDrawer,
     NavigationDrawerItem,
-    navigationDrawerItemVariants,
-    navigationDrawerVariants,
     NavigationRail,
     NavigationRailItem,
-    navigationRailItemVariants,
-    navigationRailVariants,
     Progress,
-    progressVariants,
     Provider,
     Radio,
     RadioGroup,
-    radioVariants,
     ScrollArea,
-    scrollAreaVariants,
     SearchBar,
-    searchBarVariants,
     SegmentedButton,
     SegmentedButtonGroup,
-    segmentedButtonGroupVariants,
-    segmentedButtonVariants,
     Slider,
-    sliderVariants,
     StateLayer,
-    stateLayerVariants,
     Switch,
-    switchVariants,
-    tailwindPaletteFromColor,
-    tailwindPaletteFromImage,
     TextField,
-    textFieldVariants,
     ToggleIconButton,
-    toggleIconButtonVariants,
     Tooltip,
     TooltipRoot,
     TooltipTrigger,
-    tooltipVariants,
 };
---
+++ src/lib/variants.ts
@@ -0,0 +1,53 @@
+import { variants as button } from "./components/Button.svelte";
+import { variants as checkbox } from "./components/Checkbox.svelte";
+import { variants as divider } from "./components/Divider.svelte";
+import { variants as icon } from "./components/Icon.svelte";
+import { variants as iconButton } from "./components/IconButton.svelte";
+import { variants as list } from "./components/List.svelte";
+import { variants as listItem } from "./components/ListItem.svelte";
+import { variants as menuDivider } from "./components/MenuDivider.svelte";
+import { variants as menuItem } from "./components/MenuItem.svelte";
+import { variants as menuList } from "./components/MenuList.svelte";
+import { variants as navigationDrawer } from "./components/NavigationDrawer.svelte";
+import { variants as navigationDrawerItem } from "./components/NavigationDrawerItem.svelte";
+import { variants as navigationRail } from "./components/NavigationRail.svelte";
+import { variants as navigationRailItem } from "./components/NavigationRailItem.svelte";
+import { variants as progress } from "./components/Progress.svelte";
+import { variants as radio } from "./components/Radio.svelte";
+import { variants as scrollArea } from "./components/ScrollArea.svelte";
+import { variants as searchBar } from "./components/SearchBar.svelte";
+import { variants as segmentedButton } from "./components/SegmentedButton.svelte";
+import { variants as segmentedButtonGroup } from "./components/SegmentedButtonGroup.svelte";
+import { variants as slider } from "./components/Slider.svelte";
+import { variants as stateLayer } from "./components/StateLayer.svelte";
+import { variants as switchComponent } from "./components/Switch.svelte";
+import { variants as toggleIconButton } from "./components/ToggleIconButton.svelte";
+import { variants as tooltip } from "./components/Tooltip.svelte";
+
+export {
+    button,
+    checkbox,
+    divider,
+    icon,
+    iconButton,
+    list,
+    listItem,
+    menuDivider,
+    menuItem,
+    menuList,
+    navigationDrawer,
+    navigationDrawerItem,
+    navigationRail,
+    navigationRailItem,
+    progress,
+    radio,
+    scrollArea,
+    searchBar,
+    segmentedButton,
+    segmentedButtonGroup,
+    slider,
+    stateLayer,
+    switchComponent,
+    toggleIconButton,
+    tooltip,
+};
