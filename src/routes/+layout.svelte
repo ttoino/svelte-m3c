f768f7d7863d1fa768b746f6134b487e3aad0443
toino <me@toino.pt>
2025-11-09 23:12:30 +0000

    Improve layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+    import { Provider, ScrollArea } from "$lib/index.js";
+
     import "../app.css";
+
     let { children } = $props();
 </script>
 
-{@render children()}
+<Provider>
+    <div class="flex h-full flex-row items-stretch">
+        <ScrollArea containerClass="grow">
+            <main class="flex flex-col items-center justify-center gap-4 p-4">
+                {@render children()}
+            </main>
+        </ScrollArea>
+    </div>
+</Provider>
