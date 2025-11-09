0273670531ddb27b4573e7ca73dd22cf9b5a0cee
toino <me@toino.pt>
2025-11-09 22:24:28 +0000

    Add component directory to docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
-<h1>Welcome to your library project</h1>
-<p>
-    Create your package using @sveltejs/package and preview/showcase your work
-    with SvelteKit
-</p>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<script lang="ts">
+    import { resolve } from "$app/paths";
+    import { Button } from "$lib/index.js";
+</script>
+
+<Button href={resolve("/components")}>Components</Button>
