5842dbb4cf66e84dcd4397f83bc678b1d77b6f1e
toino <me@toino.pt>
2025-11-10 17:00:53 +0000

    Add thumbnail for radio and checkbox

---
+++ src/routes/thumbnail/checkbox/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { Checkbox } from "$lib/index.js";
+
+    import Device from "../Device.svelte";
+    import Thumbnail from "../Thumbnail.svelte";
+</script>
+
+<Thumbnail>
+    <Device containerClass="gap-8 py-48 -m-32 ml-32 w-full px-8">
+        <h2 class="text-title-l">Appearance</h2>
+
+        <div class="flex flex-row items-center gap-12 text-body-l">
+            <Checkbox />
+            Dark mode
+        </div>
+        <div class="flex flex-row items-center gap-12 text-body-l">
+            <Checkbox checked />
+            Large text mode
+        </div>
+    </Device>
+</Thumbnail>
---
+++ src/routes/thumbnail/radio/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Radio, RadioGroup } from "$lib/index.js";
+
+    import Device from "../Device.svelte";
+    import Thumbnail from "../Thumbnail.svelte";
+</script>
+
+<Thumbnail>
+    <Device containerClass="gap-8 py-48 -m-32 ml-32 w-full px-8">
+        <h2 class="text-title-l">Language</h2>
+
+        <RadioGroup class="contents" value="en">
+            <div class="flex flex-row items-center gap-12 text-body-l">
+                <Radio id="focus" value="en" />
+                English
+            </div>
+            <div class="flex flex-row items-center gap-12 text-body-l">
+                <Radio value="cn" />
+                Chinese (Mandarin)
+            </div>
+        </RadioGroup>
+    </Device>
+</Thumbnail>
