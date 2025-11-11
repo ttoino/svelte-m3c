7927f93cfdc08457e2fb1a14c6032070e2e7c3a4
toino <me@toino.pt>
2025-11-11 01:02:08 +0000

    Add navigation rail thumbnail

---
+++ src/routes/thumbnail/navigation-rail/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import {
+        IconButton,
+        NavigationRail,
+        NavigationRailItem,
+    } from "$lib/index.js";
+
+    import Device from "../Device.svelte";
+    import Thumbnail from "../Thumbnail.svelte";
+</script>
+
+{#snippet column(expanded: boolean)}
+    <div class="relative h-72 overflow-hidden">
+        <Device
+            containerClass="absolute -top-16 -right-12 -bottom-48 left-12 flex-row"
+        >
+            <NavigationRail {expanded}>
+                <IconButton
+                    color="none"
+                    containerClass="mx-7 mb-4 self-start"
+                    icon="menu"
+                    variant="text"
+                />
+
+                <!-- TODO: FAB -->
+                <IconButton
+                    containerClass="mx-5 mb-12 self-start"
+                    icon="edit"
+                    shape="square"
+                    size="medium"
+                />
+
+                <NavigationRailItem active href="" icon="stars">
+                    Label
+                </NavigationRailItem>
+                <NavigationRailItem href="" icon="stars">
+                    Label
+                </NavigationRailItem>
+                <NavigationRailItem href="" icon="stars">
+                    Label
+                </NavigationRailItem>
+            </NavigationRail>
+        </Device>
+    </div>
+{/snippet}
+
+<Thumbnail class="grid grid-cols-2">
+    {@render column(false)}
+    {@render column(true)}
+</Thumbnail>
